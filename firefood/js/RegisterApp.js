30a18809cde9bd6417c3da5ea96fc8462d33b7cf
nguyenling <nguyenlinh982001@gmail.com>
2021-04-12 10:32:56 +0700

    bo sung phan js vao cho HomePage

--- firefood/js/RegisterApp.js
+++ firefood/js/RegisterApp.js
@@ -8,7 +8,7 @@ let alert_success = document.getElementsByClassName('alert-success')[0];
 let url = window.location.search;
 let urlSearch = new URLSearchParams(url);
 let success = urlSearch.get("success");
-console.log('Danh ky ');
+
 if (success == "1") {
     console.log('dang ky thanh cong');
     alert_success.style.display = 'block';
@@ -22,10 +22,16 @@ function handelSubmit(e) {
         return false;
     }
     if (email.value == '') {
-        box_alert.innerHTML = 'Vui lòng nhập tài khoản';
+        box_alert.innerHTML = 'Vui lòng nhập email';
         box_alert.style.display = 'block';
         return false;
     }
+/*    if (validateEmail(email.value) == false) {
+       
+        box_alert.innerHTML = 'Định dạng email không đúng';
+        box_alert.style.display = 'block';
+        return false;
+    }*/
 
     if (username.value == '') {
         box_alert.innerHTML = 'Vui lòng nhập tên người dùng';
@@ -47,6 +53,16 @@ function handelSubmit(e) {
         box_alert.style.display = 'block';
         return false;
     }
+
     box_alert.innerHTML = '';
     return true;
 }
+
+/*
+function validateEmail(check) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(check);
+}
+
+
+*/
